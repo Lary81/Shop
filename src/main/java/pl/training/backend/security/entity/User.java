8f5d7cec87bc162314286d7fde5274a2f2e1e7cc
lary81 <lary81@interia.pl>
2017-07-25 13:54:28 +0200

    aa

--- src/main/java/pl/training/backend/security/entity/User.java
+++ src/main/java/pl/training/backend/security/entity/User.java
@@ -19,6 +19,10 @@ public class User implements Serializable, UserDetails {
     @GeneratedValue
     private Long id;
     @Column(nullable = false)
+    private String nazwa;
+    @Column(nullable = false)
+    private int telefon;
+    @Column(nullable = false)
     private String login;
     @Column(nullable = false)
     private String password;
@@ -38,6 +42,14 @@ public class User implements Serializable, UserDetails {
     @Override
     public String getPassword() {
         return password;
+    }
+    @Override
+    public String getNazwa() {
+        return nazwa;
+    }
+    @Override
+    public int getTelefon() {
+        return telefon;
     }
 
     @Override
---
+++ src/main/java/pl/training/backend/security/service/MailService.java
@@ -0,0 +1,29 @@
+package pl.training.backend.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+import pl.training.backend.security.entity.User;
+
+
+    @Service
+    public class MailService {
+
+        @Autowired
+        private MailSender mailSender;
+
+
+        public void sendEmail(User user) {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("gryzipiorka@interia.pl");
+
+
+            message.setSubject("Wiadomość od :"+user.getNazwa());
+            message.setText("Wiadomość testowa");
+
+            mailSender.send(message);
+        }
+
+    }
+
