a1c3e5cb270eccd5fab1201d44bd69663d04d174
lary81 <lary81@interia.pl>
2017-07-27 15:22:31 +0200

    product list

---
+++ client/src/app/shop/products/product/product.component.ts
@@ -0,0 +1,31 @@
+import {Component} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Product} from '../products';
+import {ProductsService} from '../products.service';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'products',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent  {
+
+
+  // products: Observable<[Product]>
+
+  prod = [];
+
+
+  // constructor(  private productsService: ProductsService, activatedRoute: ActivatedRoute) {
+  //    this.productsService.getAll().subscribe(response => console.log(response))
+  //   console.log(activatedRoute.snapshot.data)
+  // }
+  constructor(  private productsService: ProductsService, activatedRoute: ActivatedRoute) {
+     this.productsService.getAll().subscribe(response => {
+       this.prod = response.products
+     })
+  }
+
+
+}
